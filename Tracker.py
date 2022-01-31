ce86092df30d6801289ff6fec271bcbeb4915eec
Kamil Wojnowski <kamilwojn@gmail.com>
2022-01-31 16:43:51 -0500

    imports set up

--- Tracker.py
+++ Tracker.py
@@ -1,8 +1,30 @@
 import pandas as pd
 import os
 import requests
-from dotenv import load_dotenv
+from pathlib import Path
+import panel as pn
+pn.extension('plotly')
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 
-pandas.read_excel
+print("Enter File Path")
+file_getter = Path(input())
+data = pd.read_excel(file_getter)
+data.head()
+
+
+
+#crypto_portfolio_analysis = pn.Tabs(
+    #("Housing Units Per Year", housing_units_per_year()),
+    #("Average Housing Costs in San Francisco Per Year", average_housing_costs_column),
+    #("Average Prices By Neighborhood", average_prices_neighborhood_column),
+    #("Top 10 Most Expensive Neighborhoods", most_expensive_column), 
+    #("Neighborhood Map", neighborhood_map()),
+    #name="Crypto Portfolio"
+#)   
+
+
+# Serve the# dashboard
+#market_analysis.servable()
+
+
