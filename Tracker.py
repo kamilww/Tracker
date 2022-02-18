0a1e2705f3937c7b56cabf971792af95e0705c37
Kamil Wojnowski <kamilwojn@gmail.com>
2022-02-18 15:30:49 -0500

    sum and owner calculation and investments by figures

--- Tracker.py
+++ Tracker.py
@@ -50,8 +50,52 @@ data['owner3 token value'] = data['owner3'] * data['current price']
 data['owner4 token value'] = data['owner4'] * data['current price']
 data['total'] = data['owner1 token value'] + data['owner2 token value'] + data['owner3 token value'] + data['owner4 token value']
 
-print(data)
-#generate plots
+#calculating portfolio value per owner and total percent per owner
+total_portfolio = data['total'].sum()
+owner1_total = data['owner1 token value'].sum()
+owner2_total = data['owner2 token value'].sum()
+owner3_total = data['owner3 token value'].sum()
+owner4_total = data['owner4 token value'].sum()
+
+percent_owner1 = (owner1_total / total_portfolio) * 100
+percent_owner2 = (owner2_total / total_portfolio) * 100
+percent_owner3 = (owner3_total / total_portfolio) * 100
+percent_owner4 = (owner4_total / total_portfolio) * 100
+
+#peak to trough calculation:
+
+
+#calculating investments by figures
+six_fig = 0
+five_fig = 0
+four_fig = 0
+three_fig = 0
+two_fig = 0
+one_fig = 0
+less_than_one_fig = 0
+
+for entry in data['total']:
+    if entry >= 100000:
+        six_fig+=1
+    elif entry >= 10000:
+        five_fig+=1
+    elif entry >= 1000:
+        four_fig+=1
+    elif entry >= 100:
+        three_fig+=1
+    elif entry >= 10:
+        two_fig+=1
+    elif entry >= 1:
+        one_fig+=1
+    else:
+        less_than_one_fig+=1
+
+#calculating top ten holdings
+data_sorted = data.copy()
+data_sorted.sort_values(by = 'total', ascending=False, inplace = True)
 
 
 
+#calculating top ten values by ecosystem
+
+#calculating % of supply emitted
